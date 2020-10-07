be9e3734ace16f3574a0df425f4920c34bacb551
Alexander Cronin <alexander_cronin@outlook.com>
2020-10-07 00:04:42 -0700

    Wrote JavaScript/NUMBERS.js

---
+++ JavaScript/NUMBERS.js
@@ -0,0 +1,34 @@
+// Numbers:
+// JavaScript uses "numbers", rather than 'float', or 'int'
+// Javascript is a more 'loosely typed' language in comparison to C based languages
+
+let number = 10;
+let otherNumber = 200.00986;
+console.log(number);
+console.log(otherNumber);
+// Float and int are the same type in JavaScript, therefore can be used interchangibly.
+let doSomeStrangemath = number + otherNumber * 3 - 10 * 0.1;
+console.log(number + " + " + otherNumber + " + " + 3 + " - " + 10 + " * " + 0.1 + " = " + doSomeStrangemath);
+
+// Fractions will be converted into decimal form
+// Example:
+let fraction = 7 / 8;
+let threeQuarters = 3 / 4;
+let oneThreeQuarthers = 1 + 3 / 4;
+let improperFraction = 15 / 10;
+let improperFraction2 = 5 / 4;
+
+console.log(fraction);
+console.log(threeQuarters);
+console.log(oneThreeQuarthers);
+console.log(improperFraction);
+console.log(improperFraction2);
+
+// Negetive Numbers:
+// Example:
+let negetiveNumber = -45;
+console.log(negetiveNumber);
+
+// Subtracting a greater number, from a smaller number will result in a negetive number as well
+let inTheRed = 10000 - 30000;
+console.log(inTheRed);
